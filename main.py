6cf2204d2dc88712bece2cdc03fb03e37b7d922c
Eric Dennison <ericdennison@ericdennison.local>
2011-11-20 22:00:48 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+__author__ = 'ericdennison'
+
+import twisted
+import pygame
+import argparse
+
+print "Hello, world!"
+
+print "Rama Lama Ding Dong"
+
+print "Now I am a git project"
+
+print "This is a new line"
+
+print "Now this is not in git!"
